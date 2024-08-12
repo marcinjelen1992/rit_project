50f0720be0b0b4cd41e2b3ceb3bcab33672eb722
marcinjelen1992 <marcin.jelen.1992@gmail.com>
2024-08-12 21:57:27 +0200

    finished project commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import { useState } from "react";
 import "./App.css";
 import logo from "./Logo_poziom_kolor.svg";
 import jsLogo from "./Components/Icons/js.svg";
@@ -6,11 +5,6 @@ import tsLogo from "./Components/Icons/ts.svg";
 import reactLogo from "./Components/Icons/react.svg";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  // import { RoundImage } from "./Components/RoundImage/RoundImage";
-  // comments
-
   return (
     <>
       <div className="rit">
@@ -159,7 +153,7 @@ function App() {
                 textAlign: "center",
               }}
             >
-              Dla kogo?
+              Jak uczymy, czyli dlaczego Rockin'iT?
             </div>
             <div>
               <p
@@ -167,32 +161,35 @@ function App() {
                   textAlign: "center",
                 }}
               >
-                Jesteśmy prężnie rozwijającą się firmą oferującą zróżnicowane
-                szkolenia dla osób zainteresowanych światem IT
+                Nasz system nauczania jest oparty na połączeniu edukacji z
+                praktyką. Dzięki nam poznasz jak kodować i stworzysz swój
+                pierwszy projekt!
               </p>
               <div>
                 <div className="columnModule">
-                  <h3>Studenci informatyki</h3>
+                  <h3>Ciekawe podejście do nauczania</h3>
                   <p>
-                    Skończyłeś studia, lecz chciałbyś pogłębić temat jeszcze
-                    bardziej? Oferujemy zaawansowane kursy dla osób mających
-                    edukacyjne tło w dziedzinie IT.
+                    Chcesz stworzyć stronę internetową przedstawiającą swoje
+                    artystyczne portfolio? Chcesz stworzyć pierwszą grę
+                    komputerową? U nas się tego nauczysz!
                   </p>
                 </div>
                 <div className="columnModule">
-                  <h3>Pasjonaci IT</h3>
+                  <h3>Wsparcie profesjonalisnych trenerów</h3>
                   <p>
-                    Od dzieciństwa pasjonowałeś się komputerami i chcesz wejść
-                    do większego świata IT? U nas znajdziesz odpowiednie kursy,
-                    które to umożliwią
+                    Napotkasz w trakcie realizacji projektu problem z kodem? Nie
+                    obawiaj się. U nas będziesz mógł z pomocą trenera krok po
+                    kroku prześledzić, które elementy kodu mogą wywoływać błędy
+                    i jak można je poprawić.
                   </p>
                 </div>
                 <div className="columnModule">
-                  <h3>Szukający pracy</h3>
+                  <h3>Nauka bez oceniania</h3>
                   <p>
-                    Znudziła się dotychczasowa praca lub niezadowalają cię
-                    zarobki? Nauczymy cię od podstaw jak stać się
-                    profesjonalistą w tworzeniu stron internetowych
+                    Masz złe wspomnienia ze szkoły lub obawiasz się bycia
+                    niedocenionym? U nas nie oceniamy! Możesz być pewien, że
+                    nasi trenerzy okażą przyjacielskie podejście i pomogą w
+                    rozwiązaniu problemów z kodem.
                   </p>
                 </div>
               </div>
@@ -200,15 +197,119 @@ function App() {
           </div>
         </div>
 
-        <div>
-          <button onClick={() => setCount((count) => count + 1)}>
-            count is {count}
-          </button>
-          <p>
-            Edit <code>src/App.jsx</code> and save to test HMR
-          </p>
+        <div className="fourthSection">
+          <div>
+            <div
+              style={{
+                fontSize: "32px",
+                fontWeight: "bold",
+                textAlign: "center",
+              }}
+            >
+              O nas
+            </div>
+            <div>
+              <p
+                style={{
+                  textAlign: "center",
+                }}
+              >
+                Pełnimy rolę w największej w Polsce organizacji z zakresu
+                edukacji prywatnej. Łączymy praktykę z pasją i ciągle podnosimy
+                kwalifikacje absolwentów. Dzięki nam będziecie gwiazdami{" "}
+                <span
+                  style={{
+                    color: "#FE5407",
+                  }}
+                >
+                  Rocka
+                </span>{" "}
+                w dziedzinie IT! Zatrudniamy ponad{" "}
+                <span
+                  style={{
+                    color: "#FE5407",
+                  }}
+                >
+                  40 trenerów
+                </span>{" "}
+                z ponad{" "}
+                <span
+                  style={{
+                    color: "#FE5407",
+                  }}
+                >
+                  10 latami
+                </span>{" "}
+                doświadczenia. Współpracuje z nami{" "}
+                <span
+                  style={{
+                    color: "#FE5407",
+                  }}
+                >
+                  15 firm
+                </span>{" "}
+                w{" "}
+                <span
+                  style={{
+                    color: "#FE5407",
+                  }}
+                >
+                  9 miastach
+                </span>
+                . Poznaj naszych trenerów!
+              </p>
+              <div>
+                <div className="columnModule">
+                  <h3>Maciej Dojlido</h3>
+                  <p>
+                    Inżynier oprogramowania full-stack (JS, React, PHP, MySQL) z
+                    8-letnim doświadczeniem
+                  </p>
+                </div>
+                <div className="columnModule">
+                  <h3>Karolina Saukens</h3>
+                  <p>
+                    Nauczyciel akademicki na kierunku Zarządzanie i Psychologia
+                    w Biznesie
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-        <p>Click on the Vite and React logos to learn more</p>
+
+        <div className="secondSection">
+          <div>
+            <p
+              style={{
+                width: "480px",
+              }}
+            >
+              <span>Rockin'iT sp. z o.o.</span>
+            </p>
+            <p>al. Zwycięstwa 96 / 98</p>
+            <p>81-451 Gdynia, Polska</p>
+            <a
+              class="button"
+              href="mailto:letsrock@rockinit.pl"
+              style={{
+                color: "#FE5407",
+                margin: "0 0 32px 0",
+                display: "block",
+                backgroundColor: "#FFFFFF",
+                width: "280px",
+                height: "32px",
+              }}
+            >
+              Kliknij żeby napisać do nas maila
+            </a>
+          </div>
+          <div className="logoBoxes">
+            <div className="logoBoxesRow">
+              <div className="floatingblack"></div>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
